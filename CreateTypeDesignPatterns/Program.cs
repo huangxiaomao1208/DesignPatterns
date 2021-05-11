67a91346376d27f84fe10060be7e7792b0887512
Huangxiaomao <Huangxiaomao1208@163.com>
2021-05-11 22:37:12 +0800

    23种设计模式

---
+++ CreateTypeDesignPatterns/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary>
+/// 创建型设计模式
+/// </summary>
+namespace CreateTypeDesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CreateTypeDesignPatterns/SingletonPattern.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CreateTypeDesignPatterns
+{
+    /// <summary>
+    /// 单例模式
+    /// </summary>
+    public class SingletonPattern
+    {
+    }
+}
