6a13a4b7339c143ae3a6ce9ae15cb840e234e1f7
Huangxiaomao <Huangxiaomao1208@163.com>
2021-05-11 22:45:41 +0800

    单例模式

---
+++ CreateTypeDesignPatterns/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CreateTypeDesignPatterns
+{
+    /// <summary>
+    /// 单例模式
+    /// </summary>
+    public class Singleton
+    {
+        private Singleton()
+        { 
+        }
+        public static Singleton _instance = null;
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+       
+    }
+}
