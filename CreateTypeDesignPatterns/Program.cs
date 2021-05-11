930560ccf0c18a6c4f02f5ec393660fc1755a759
Huangxiaomao <Huangxiaomao1208@163.com>
2021-05-11 23:32:54 +0800

    Thread Safe Singleton patterns

--- CreateTypeDesignPatterns/Program.cs
+++ CreateTypeDesignPatterns/Program.cs
@@ -9,7 +9,23 @@ namespace CreateTypeDesignPatterns
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region 单例模式
+            Console.WriteLine("********** 单例模式 *********");
+            Console.WriteLine("********** 单例模式-非线程安全 *********");
+            Singleton singleton1 = Singleton.GetInstance();
+            Singleton singleton2 = Singleton.GetInstance();
+            Console.WriteLine(object.ReferenceEquals(singleton1, singleton2));
+            Console.WriteLine("********** 单例模式-线程安全 *********");
+            SingletonThreadSafe singleton3 = SingletonThreadSafe.GetInstance();
+            SingletonThreadSafe singleton4 = SingletonThreadSafe.GetInstance();
+            Console.WriteLine(object.ReferenceEquals(singleton3, singleton4));
+
+
+
+            #endregion
+
+            Console.ReadLine();
+
         }
     }
 }
--- CreateTypeDesignPatterns/Singleton.cs
+++ CreateTypeDesignPatterns/Singleton.cs
@@ -5,14 +5,15 @@ using System.Text;
 namespace CreateTypeDesignPatterns
 {
     /// <summary>
-    /// 单例模式
+    /// 单例模式-非线程安全
     /// </summary>
     public class Singleton
     {
         private Singleton()
         { 
         }
-        public static Singleton _instance = null;
+        private static Singleton _instance = null;
+
         public static Singleton GetInstance()
         {
             if (_instance == null)
@@ -22,5 +23,42 @@ namespace CreateTypeDesignPatterns
             return _instance;
         }
        
+    }
+
+    
+    /// <summary>
+    /// 单例模式-线程安全
+    /// </summary>
+    public class SingletonThreadSafe
+    {
+        private SingletonThreadSafe()
+        { 
+        }
+
+        private static SingletonThreadSafe _instance = null;
+
+        private static readonly object obj = new object();
+
+        public static SingletonThreadSafe GetInstance()
+        {
+            //双IF加锁  IF-LOCK-IF
+
+            //第一个IF判断该对象有没有被实例化
+            if (_instance == null)
+            {
+                //如果没有被实例化，加锁，这个时刻只允许一个用户进去实例化,其他的用户等待
+                lock (obj)
+                {
+                    //判断有没有用户已经实例化了，因为在lock等待的时候，排在前面的肯定已经实例化成功了，所以这里也要加上IF二次判断一下
+                    if (_instance == null)
+                    {
+                        _instance = new SingletonThreadSafe();
+                    }
+                }
+            }
+            return _instance;
+        }
+
+        
     }
 }
