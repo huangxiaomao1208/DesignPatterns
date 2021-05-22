e27238616348f75565ab16047e8172c47e35c34e
Huangxiaomao <Huangxiaomao1208@163.com>
2021-05-22 16:15:00 +0800

    简单工厂

--- CreateTypeDesignPatterns/Program.cs
+++ CreateTypeDesignPatterns/Program.cs
@@ -10,18 +10,25 @@ namespace CreateTypeDesignPatterns
         static void Main(string[] args)
         {
             #region 单例模式
-            Console.WriteLine("********** 单例模式 *********");
-            Console.WriteLine("********** 单例模式-非线程安全 *********");
-            Singleton singleton1 = Singleton.GetInstance();
-            Singleton singleton2 = Singleton.GetInstance();
-            Console.WriteLine(object.ReferenceEquals(singleton1, singleton2));
-            Console.WriteLine("********** 单例模式-线程安全 *********");
-            SingletonThreadSafe singleton3 = SingletonThreadSafe.GetInstance();
-            SingletonThreadSafe singleton4 = SingletonThreadSafe.GetInstance();
-            Console.WriteLine(object.ReferenceEquals(singleton3, singleton4));
+            //Console.WriteLine("********** 单例模式 *********");
+            //Console.WriteLine("********** 单例模式-非线程安全 *********");
+            //Singleton singleton1 = Singleton.GetInstance();
+            //Singleton singleton2 = Singleton.GetInstance();
+            //Console.WriteLine(object.ReferenceEquals(singleton1, singleton2));
+            //Console.WriteLine("********** 单例模式-线程安全 *********");
+            //SingletonThreadSafe singleton3 = SingletonThreadSafe.GetInstance();
+            //SingletonThreadSafe singleton4 = SingletonThreadSafe.GetInstance();
+            //Console.WriteLine(object.ReferenceEquals(singleton3, singleton4));
+            #endregion
 
 
-
+            #region 简单工厂
+            Product product1 = SimpleFactory.CreateProduct("1");
+            product1.Show();
+            Product product2 = SimpleFactory.CreateProduct("2");
+            product2.Show();
+            Product product3 = SimpleFactory.CreateProduct("3");
+            product3.Show();
             #endregion
 
             Console.ReadLine();
---
+++ CreateTypeDesignPatterns/SimpleFactory.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CreateTypeDesignPatterns
+{
+    /// <summary>
+    /// 简单工厂模式
+    /// 用一个工厂创建不同类型的对象
+    /// 优点：上端传递一个对象类型，就可以得到相对应的对象
+    /// 缺点：如果新增一个对象，需要修改工厂创建对象的方法
+    /// </summary>
+    public class SimpleFactory
+    {
+        public static Product CreateProduct(string type)
+        {
+            Product product = null;
+            switch (type)
+            {
+                case "1":
+                    product = new Product1();
+                    break;
+                case "2":
+                    product = new Product2();
+                    break;
+                case "3":
+                    product = new Product3();
+                    break;
+            }
+            return product;
+        }
+    }
+    /// <summary>
+    /// 接口  这里可以使用抽象类代替
+    /// </summary>
+    public interface Product
+    {
+        void Show();
+    }
+
+    public class Product1 : Product
+    {
+        public void Show()
+        {
+            Console.WriteLine("Product1 - Show");
+        }
+    }
+
+    public class Product2 : Product
+    {
+        public void Show()
+        {
+            Console.WriteLine("Product2 - Show");
+        }
+    }
+    public class Product3 : Product
+    {
+        public void Show()
+        {
+            Console.WriteLine("Product3 - Show");
+        }
+    }
+}
